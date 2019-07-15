17a33309754477127e14dd23d89358d036f1cffb
Aunyarat Champreeda <aunyarat.juju@gmail.com>
2019-07-15 22:50:14 +0700

    Add image cover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileCoverComponent } from './components/profile-cover/profile-cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileCoverComponent
   ],
   imports: [
     BrowserModule,
