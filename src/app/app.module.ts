dc57d53f4d62fd2046693c13d5322f668b5a3971
Aunyarat Champreeda <aunyarat.juju@gmail.com>
2019-07-16 22:35:55 +0700

    Add basic info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileCoverComponent } from './components/profile-cover/profile-cover.component';
+import { BasicInformationComponent } from './components/basic-information/basic-information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileCoverComponent
+    ProfileCoverComponent,
+    BasicInformationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/basic-information/basic-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'basic-information',
+  templateUrl: './basic-information.component.html',
+  styleUrls: ['./basic-information.component.scss']
+})
+export class BasicInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
