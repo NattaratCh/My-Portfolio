9916bc8b396ac986d1ada99a650d268af61b3033
unknown <ning.nattarat@gmail.com>
2019-08-05 21:51:50 +0700

    Add application screen

--- src/app/components/profile-cover/profile-cover.component.ts
+++ src/app/components/profile-cover/profile-cover.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleAnalyticsServiceService } from 'src/app/services/google-analytics-service.service';
 
 @Component({
   selector: 'profile-cover',
   templateUrl: './profile-cover.component.html',
   styleUrls: ['./profile-cover.component.scss']
 })
+
 export class ProfileCoverComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gaService: GoogleAnalyticsServiceService) { }
 
   ngOnInit() {
+  }
+
+  onCVClick() {
+    this.gaService.sendEvent('clickCV', {})
   }
 
 }
